2b0627a40ece32bee9a71417a90b49719c643a8b
Rex Brooks <rexb@starbourne.com>
2018-05-28 08:39:23 -0700

    Add files via upload

---
+++ edxl_cap_v1_2/Models/EdxlCapMsg.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+using edxl_cap_v1_2.Models;
+
+namespace edxl_cap_v1_2.Models
+{
+    public class EdxlCapMsg
+    {
+        [Key]
+        public int Id { get; set; }
+
+        public string Alert_Identifier { get; set; }
+
+        public Alert Alert { get; set; }
+
+        public Info Info { get; set; }
+
+        public Area Area { get; set; }
+
+        public Resource Resource { get; set; }
+
+        public IEnumerator GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+    }
+
+}
