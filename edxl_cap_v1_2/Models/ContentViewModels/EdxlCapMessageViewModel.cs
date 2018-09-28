208f7250179a275d678b431c863e32ba5adc930f
Rex Brooks <rexb@starbourne.com>
2018-09-28 12:23:51 -0700

    Add files via upload

--- edxl_cap_v1_2/Models/ContentViewModels/EdxlCapMessageViewModel.cs
+++ edxl_cap_v1_2/Models/ContentViewModels/EdxlCapMessageViewModel.cs
@@ -1,16 +1,31 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Threading.Tasks;
 using edxl_cap_v1_2.Models;
+using Microsoft.AspNetCore.Mvc.Rendering;
 
 namespace edxl_cap_v1_2.Models.ContentViewModels
 {
     public class EdxlCapMessageViewModel
     {
+        public EdxlCapMessageViewModel()
+        {
+        }
+
+        public EdxlCapMessageViewModel(string v, object p)
+        {
+        }
+
         [Key]
+        public int AlertIndex { get; set; }
         public string Alert_Identifier { get; set; }
+        public int SelectedAlertIndex { get; set; }
+        [NotMapped]
+        public List<SelectListItem> Alert_Identifiers { get; set; }
+        public List<AlertVm> Alerts { get; set; }
 
         public Alert Alert { get; set; }
 
@@ -20,4 +35,7 @@ namespace edxl_cap_v1_2.Models.ContentViewModels
 
         public Resource Resource { get; set; }
     }
+
+
+
 }
